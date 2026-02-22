a9cf3726c8e27823a8746b1b8cbad796ccb24be3
Jose Gomez <gomezgaona@gmail.com>
2026-02-22 04:09:54 +0000

    intermezzo

--- experiments/FCT_BDP_SFQ/plot_CDF_FCT.py
+++ experiments/FCT_BDP_SFQ/plot_CDF_FCT.py
@@ -108,9 +108,9 @@ def main():
         ax.step(x_b, y_b, where="post", label=lbl_b, color="#82AA45")
         ax.step(x_p, y_p, where="post", label=lbl_p, color="#95253B")
 
-        ax.set_xlim(220, 3100)
+        ax.set_xlim(0, 60)
 
-        ax.set_xlabel("FCT (seconds)")
+        ax.set_xlabel("FCT [seconds]")
         ax.set_ylabel("CDF")
         ax.grid(True)
 
